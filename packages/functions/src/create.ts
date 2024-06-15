337d10140d5f80cdf77c970283b18c062728d7e8
cgle04 <crglenn04@gmail.com>
2024-06-15 15:27:26 -0700

    adding the API

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,33 @@
+import * as uuid from "uuid";
+import { Table } from "sst/node/table";
+import handler from "@notes/core/handler";
+import dynamoDb from "@notes/core/dynamodb";
+
+export const main = handler(async (event) => {
+    let data = {
+        content: "",
+        attachment: "",
+    };
+
+    if(event.body != null) {
+        data = JSON.parse(event.body);
+    }
+
+    const params = {
+        TableName: Table.Notes.tableName,
+        Item: {
+            //the attributes of the item to be created
+            // the attrs of the item to be created
+            userId: "123",
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        },
+    };
+
+    await dynamoDb.put(params);
+
+    return JSON.stringify(params.Item);
+});
+   
