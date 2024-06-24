abf7e06099deebbd3c4c9c9cc852f25d20f93565
cgle04 <crglenn04@gmail.com>
2024-06-24 13:54:18 -0700

    setting up react app

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Scrrrrt</h1>
+                <p className="text-muted">scrt scrrrt</p>
+            </div>
+        </div>
+    );
+}
---
+++ packages/frontend/src/containers/NotFound.tsx
@@ -0,0 +1,9 @@
+import "./NotFound.css";
+
+export default function NotFound() {
+    return (
+        <div className="NotFound text-center">
+            <h3>BITCH you thought</h3>
+        </div>
+    );
+}
